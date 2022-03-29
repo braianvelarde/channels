c8639d0bd782d69209e402efd34773c3f918dd3b
Braian Velarde <braianvelarde@gmail.com>
2022-03-29 12:38:01 -0300

    add viewport meta tag

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home({ channels }) {
           <title>Grilla de canales</title>
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+            content="width=device-width, user-scalable=no"
           ></meta>
           <meta
             name="theme-color"
