0a2c000eac40bf4df27ff5134d394a8a1a34f47a
Braian Velarde <braianvelarde@gmail.com>
2022-03-29 13:59:27 -0300

    improve lighthouse

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=9999999999, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["media.graphcms.com"],
