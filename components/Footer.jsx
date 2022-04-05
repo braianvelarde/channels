f3950674399aeb081f58faa7075c64c38b8d5fa5
Braian Velarde <braianvelarde@gmail.com>
2022-04-05 10:48:44 -0300

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className=" mt-4 mx-4 text-sm">
+      <p>
+        Este sitio no tiene relacion con ninguna de las empresas proveedoras del
+        servicio.
+      </p>
+      <p>
+        La información brindada por este sitio puede no ser la correcta para su
+        proveedor.
+      </p>
+      <p>&copy; Copyright 2022</p>
+    </footer>
+  );
 }
